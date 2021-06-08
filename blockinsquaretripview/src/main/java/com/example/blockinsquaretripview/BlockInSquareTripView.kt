49e356873716b8af51a68533f03b34875c17283b
Anwesh43 <anweshthecool0@gmail.com>
2021-06-09 02:06:47 +0530

    adding constants and libraries

--- blockinsquaretripview/src/main/java/com/example/blockinsquaretripview/BlockInSquareTripView.kt
+++ blockinsquaretripview/src/main/java/com/example/blockinsquaretripview/BlockInSquareTripView.kt
@@ -1,2 +1,27 @@
 package com.example.blockinsquaretripview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val blockSizeFactor : Float = 15.9f
