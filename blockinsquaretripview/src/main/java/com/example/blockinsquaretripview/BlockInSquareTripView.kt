a6c266a9445947617e3f7bfd787cbbb4e4edc4f8
Anwesh43 <anweshthecool0@gmail.com>
2021-06-09 02:49:15 +0530

    adding BISTNode class

--- blockinsquaretripview/src/main/java/com/example/blockinsquaretripview/BlockInSquareTripView.kt
+++ blockinsquaretripview/src/main/java/com/example/blockinsquaretripview/BlockInSquareTripView.kt
@@ -131,4 +131,45 @@ class BlockInSquareTripView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BISTNode(var i : Int, val state : State = State()) {
+
+        private var next : BISTNode? = null
+        private var prev : BISTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BISTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBISTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BISTNode {
+            var curr : BISTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
